429f4c2e1b6d26dbfb6611be4e6696a4c0df8423
Yash Sharma <ysharma@squarespace.com>
2020-12-01 22:51:28 -0500

    day 1 complete

--- day1/script.py
+++ day1/script.py
@@ -8,16 +8,23 @@
 with open("input.txt", "r") as file:
     nums = [int(line) for line in file.readlines()]
 
-#create storage variables
-num1 = 0
-num2 = 0
-
 #find 2 nums whose sum is 2020
-for x in nums:
-    for y in nums:
-        if (x+y) == 2020:
-            num1 = x
-            num2 = y
+def solution1():
+    for x in nums:
+        for y in nums:
+            if (x+y) == 2020:
+                return(x*y)
 
-#multipy the 2 nums and get answer for part 1
-print('Part 1 Solution: ' + str(num1 * num2))
+#answer part 1
+print('Part 1 Solution: ' + str(solution1()))
+
+#find 3 nums whose sum is 2020
+def solution2():
+    for x in nums:
+        for y in nums:
+            for z in nums:
+                if (x+y+z) == 2020:
+                    return(x*y*z)
+
+#answer part 2
+print('Part 2 Solution: ' + str(solution2()))

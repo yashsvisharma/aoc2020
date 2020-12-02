4b6217e5e21d2e7cb9cfcd564aaaa264f4311db8
Yash Sharma <ysharma@squarespace.com>
2020-12-01 22:05:26 -0500

    found solution part 1

--- day1/script.py
+++ day1/script.py
@@ -2,8 +2,22 @@
 # Yash Sharma
 # me@yashsharma.com
 
-with open("input.txt") as file:
-    nums = file.readlines()
-    nums = [x.strip() for x in nums]
+# find 2 nums that sum 2020 and return product
 
-print(nums)
+# read file into list of ints called nums
+with open("input.txt", "r") as file:
+    nums = [int(line) for line in file.readlines()]
+
+#create storage variables
+num1 = 0
+num2 = 0
+
+#find 2 nums whose sum is 2020
+for x in nums:
+    for y in nums:
+        if (x+y) == 2020:
+            num1 = x
+            num2 = y
+
+#multipy the 2 nums and get answer
+print(num1 * num2)

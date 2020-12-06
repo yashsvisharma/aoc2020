86b9a0ebb932491d07aa0f2797a096cb4d92723f
Yash Sharma <ysharma@squarespace.com>
2020-12-05 21:22:50 -0500

    day 2 solved

--- day3/script.py
+++ day3/script.py
@@ -8,13 +8,29 @@
 with open("input.txt", "r") as file:
     text = file.readlines()
 
-lineCounter = 0
-treeCounter = 0
-cursor = 0
 
-for line in text:
-    if (line[(cursor % 31)] == '#'):
-        treeCounter += 1
-    cursor += 3
+def detectTrees(right, down):
+    treeCounter = 0
+    xCursor = 0
+    yCursor = 0
+    txtLength = len(text)
 
-print(treeCounter)
+    for line in range(txtLength/down):
+        if(text[yCursor % txtLength][xCursor % 31] == '#'):
+            treeCounter += 1
+        xCursor += right
+        yCursor += down
+    return treeCounter
+
+
+# solution to part 1:
+print(detectTrees(3, 1))
+
+# solution to part 2:
+print(
+    detectTrees(1, 1) *
+    detectTrees(3, 1) *
+    detectTrees(5, 1) *
+    detectTrees(7, 1) *
+    detectTrees(1, 2)
+)
